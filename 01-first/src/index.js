d836f9dd81f828ac9118fb730b5682864d8ffff0
oleh zharovskyi <oleh.zharovskyi@raketech.com>
2020-10-21 09:30:34 +0300

    add store

--- 01-first/src/index.js
+++ 01-first/src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import state, { subscribe } from './redax/state';
-import { addPost, updateNewPostText } from './redax/state';
+import store from './redax/state';
 import { BrowserRouter } from 'react-router-dom';
 
 let rerenderTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+            <App state={store.getState()} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} />
         </BrowserRouter>, document.getElementById('root'));
 }
 
-rerenderTree(state);
-subscribe(rerenderTree);
+rerenderTree(store.getState());
+store.subscribe(rerenderTree);
--- 01-first/src/redax/state.js
+++ 01-first/src/redax/state.js
@@ -1,114 +1,112 @@
-
-let rerenderTree = () => {
-    console.log('State chnged')
-}
-
-let state = {
-    profilePage: {
-        posts: [
-            {
-                id: 1,
-                post: "HELOO"
-            },
-            {
-                id: 2,
-                post: "How are you"
-            },
-        ],
-        newPostText: 'oleh zharovskyi'
-
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {
+                    id: 1,
+                    post: "HELOO"
+                },
+                {
+                    id: 2,
+                    post: "How are you"
+                },
+            ],
+            newPostText: 'oleh zharovskyi'
+    
+        },
+        messagePage: {
+            dialogData: [
+                {
+                    id: 1,
+                    name: 'Oleh',
+                    imgSrc: 'https://www.flaticon.com/premium-icon/icons/svg/3054/3054163.svg'
+                },
+                {
+                    id: 2,
+                    name: 'Andryi',
+                    imgSrc: 'https://image.flaticon.com/icons/png/512/3054/3054341.png'
+                },
+                {
+                    id: 3,
+                    name: 'Volodja',
+                    imgSrc: 'https://www.flaticon.com/premium-icon/icons/svg/3054/3054163.svg'
+                },
+                {
+                    id: 4,
+                    name: 'Sveta',
+                    imgSrc: 'https://www.flaticon.com/premium-icon/icons/svg/706/706807.svg'
+                },
+                {
+                    id: 5,
+                    name: 'Zina',
+                    imgSrc: 'https://www.flaticon.com/svg/static/icons/svg/921/921347.svg'
+                },
+                {
+                    id: 6,
+                    name: 'Bob',
+                    imgSrc: 'https://www.flaticon.com/premium-icon/icons/svg/2667/2667378.svg'
+                }
+            ],
+            dialogMessage: [
+                {
+                    id: 1,
+                    message: 'Hi',
+    
+                },
+                {
+                    id: 2,
+                    message: 'Bra',
+    
+    
+                },
+                {
+                    id: 3,
+                    message: 'Ok sdf sdf',
+    
+                },
+                {
+                    id: 4,
+                    message: 'How are you',
+    
+                },
+                {
+                    id: 5,
+                    message: 'Hello mate',
+    
+                },
+                {
+                    id: 6,
+                    message: 'No',
+    
+                }
+            ]
+        },
     },
-    messagePage: {
-        dialogData: [
-            {
-                id: 1,
-                name: 'Oleh',
-                imgSrc: 'https://www.flaticon.com/premium-icon/icons/svg/3054/3054163.svg'
-            },
-            {
-                id: 2,
-                name: 'Andryi',
-                imgSrc: 'https://image.flaticon.com/icons/png/512/3054/3054341.png'
-            },
-            {
-                id: 3,
-                name: 'Volodja',
-                imgSrc: 'https://www.flaticon.com/premium-icon/icons/svg/3054/3054163.svg'
-            },
-            {
-                id: 4,
-                name: 'Sveta',
-                imgSrc: 'https://www.flaticon.com/premium-icon/icons/svg/706/706807.svg'
-            },
-            {
-                id: 5,
-                name: 'Zina',
-                imgSrc: 'https://www.flaticon.com/svg/static/icons/svg/921/921347.svg'
-            },
-            {
-                id: 6,
-                name: 'Bob',
-                imgSrc: 'https://www.flaticon.com/premium-icon/icons/svg/2667/2667378.svg'
-            }
-        ],
-        dialogMessage: [
-            {
-                id: 1,
-                message: 'Hi',
-
-            },
-            {
-                id: 2,
-                message: 'Bra',
-
-
-            },
-            {
-                id: 3,
-                message: 'Ok sdf sdf',
-
-            },
-            {
-                id: 4,
-                message: 'How are you',
-
-            },
-            {
-                id: 5,
-                message: 'Hello mate',
-
-            },
-            {
-                id: 6,
-                message: 'No',
-
-            }
-        ]
+    getState(){
+        return this._state;
     },
-}
-window.state = state;
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        post: state.profilePage.newPostText
+    callSubscriber() {
+        console.log('State chnged')
+    },
+    addPost () {
+        let newPost = {
+            id: 5,
+            post: this._state.profilePage.newPostText
+        }
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe (observer) {
+        this._callSubscriber = observer;
     }
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderTree(state);
 }
 
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderTree(state);
-}
 
-export const subscribe = (observer) => {
-    rerenderTree = observer;
-}
+export default store;
 
-// store OOP
-
-
-
-
-export default state;
+window.store = store;
