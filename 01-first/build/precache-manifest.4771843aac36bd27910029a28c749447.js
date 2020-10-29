9d8171bd1025c6eed844f1301dfbdf259d94f0ca
oleh zharovskyi <oleh.zharovskyi@raketech.com>
2020-10-29 13:22:08 +0200

    add home page

---
+++ 01-first/build/precache-manifest.4771843aac36bd27910029a28c749447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c12b7f75ab443333123ddf713253c2",
+    "url": "/React-social-train/01-first/build/index.html"
+  },
+  {
+    "revision": "e0a56fd5dec9f5e82f59",
+    "url": "/React-social-train/01-first/build/static/css/main.b0191ae9.chunk.css"
+  },
+  {
+    "revision": "4fbd5beb02628eea9692",
+    "url": "/React-social-train/01-first/build/static/js/2.07736a0a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-social-train/01-first/build/static/js/2.07736a0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0a56fd5dec9f5e82f59",
+    "url": "/React-social-train/01-first/build/static/js/main.f04a2448.chunk.js"
+  },
+  {
+    "revision": "80785c7f743c8def2819",
+    "url": "/React-social-train/01-first/build/static/js/runtime-main.355e951e.js"
+  }
+]);
